03d4658f63b5c2e17bd0d027b1faf61fb65d7e0b
kippy <799182081@qq.com>
2015-12-18 08:59:16 +0800

    Update timer.v
    Align the code.

--- rtl/io/timer/timer.v
+++ rtl/io/timer/timer.v
@@ -21,23 +21,23 @@ module timer (
 	/********** 总线接口 **********/
 	input  wire					cs_,	   // 片选信号
 	input  wire					as_,	   // 地址选通
-	input  wire					rw,		   // Read / Write
-	input  wire [`TIMER_ADDR_BUS] addr,	   // 地址
-	input  wire [`WordDataBus]	wr_data,   // 写数据
-	output reg	[`WordDataBus]	rd_data,   // 读取数据
+	input  wire					rw,	   // Read / Write
+	input  wire [`TIMER_ADDR_BUS]                   addr,	   // 地址
+	input  wire [`WordDataBus]	                wr_data,   // 写数据
+	output reg  [`WordDataBus]	                rd_data,   // 读取数据
 	output reg					rdy_,	   // 就绪信号
 	/********** 中断输出 **********/
-	output reg					irq		   // 中断请求（控制寄存器 1）
+	output reg					irq	   // 中断请求（控制寄存器 1）
 );
 
 	/********** 控制寄存器 **********/
 	// 控制寄存器 0 : 控制
-	reg							mode;	   // 模式
-	reg							start;	   // 起始位
+	reg						mode;	   // 模式
+	reg						start;	   // 起始位
 	// 控制寄存器 2 : 最大值
-	reg [`WordDataBus]			expr_val;  // 最大值
+	reg [`WordDataBus]			        expr_val;  // 最大值
 	// 控制寄存器 3 : 计数器
-	reg [`WordDataBus]			counter;   // 计数器
+	reg [`WordDataBus]			        counter;   // 计数器
 
 	/********** 计时完成标志位 **********/
 	wire expr_flag = ((start == `ENABLE) && (counter == expr_val)) ? `ENABLE : `DISABLE;
@@ -52,7 +52,7 @@ module timer (
 				rdy_	 <= #1 `DISABLE_;
 				start	 <= #1 `DISABLE;
 				mode	 <= #1 `TIMER_MODE_ONE_SHOT;
-				irq		 <= #1 `DISABLE;
+				irq	 <= #1 `DISABLE;
 				expr_val <= #1 `WORD_DATA_W'h0;
 				counter	 <= #1 `WORD_DATA_W'h0;
 			end 
@@ -61,11 +61,11 @@ module timer (
 				/* 准备就续绪 */
 				if ((cs_ == `ENABLE_) && (as_ == `ENABLE_)) 
 					begin
-						rdy_	 <= #1 `ENABLE_;
+						rdy_  <= #1 `ENABLE_;
 					end 
 				else 
 					begin
-						rdy_	 <= #1 `DISABLE_;
+						rdy_  <= #1 `DISABLE_;
 					end
 				/* 读访问 */
 				if ((cs_ == `ENABLE_) && (as_ == `ENABLE_) && (rw == `READ)) 
@@ -91,36 +91,36 @@ module timer (
 					end 
 				else 
 					begin
-						rd_data	 <= #1 `WORD_DATA_W'h0;
+						rd_data  <= #1 `WORD_DATA_W'h0;
 					end
 				/* 写访问 */
 				// 控制寄存器 0
 				if ((cs_ == `ENABLE_) && (as_ == `ENABLE_) && 
 					(rw == `WRITE) && (addr == `TIMER_ADDR_CTRL)) 
 					begin
-						start	 <= #1 wr_data[`TIMER_START_LOC];
-						mode	 <= #1 wr_data[`TIMER_MODE_LOC];
+						start  <= #1 wr_data[`TIMER_START_LOC];
+						mode   <= #1 wr_data[`TIMER_MODE_LOC];
 					end 
 				else if ((expr_flag == `ENABLE)	 &&
 							 (mode == `TIMER_MODE_ONE_SHOT)) 
 					begin
-						start	 <= #1 `DISABLE;
+					        start  <= #1 `DISABLE;
 					end
 				// 控制寄存器 1
 				if (expr_flag == `ENABLE) 
 					begin
-						irq		 <= #1 `ENABLE;
+						irq  <= #1 `ENABLE;
 					end 
 				else if ((cs_ == `ENABLE_) && (as_ == `ENABLE_) && 
 							 (rw == `WRITE) && (addr ==	 `TIMER_ADDR_INTR)) 
 					begin
-						irq		 <= #1 wr_data[`TimerIrqLoc];
+						irq  <= #1 wr_data[`TimerIrqLoc];
 					end
 				// 控制寄存器 2
 				if ((cs_ == `ENABLE_) && (as_ == `ENABLE_) && 
 					(rw == `WRITE) && (addr == `TIMER_ADDR_EXPR)) 
 					begin
-						expr_val <= #1 wr_data;
+						expr_val  <= #1 wr_data;
 					end
 				// 控制寄存器 3
 				if ((cs_ == `ENABLE_) && (as_ == `ENABLE_) && 
