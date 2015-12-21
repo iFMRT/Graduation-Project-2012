c22e5557d807415bc9698e91bf969d9a43978fa2
kippy <799182081@qq.com>
2015-12-21 17:43:21 +0800

    Update if_reg.v
    Add the comments of declarations

--- rtl/cpu/if_reg.v
+++ rtl/cpu/if_reg.v
@@ -1,17 +1,30 @@
-/******************************************************************************
- -- FILE NAME	: if_reg.v
+/* 
+ -- ============================================================================
+ -- FILE NAME : if_reg.v
  -- DESCRIPTION : IF/ID 流水线寄存器的实现
  -- ----------------------------------------------------------------------------
- -- Date : 2015/12/8                       Wrier : kippy
- ******************************************************************************/
+ -- Date : 2015/12/8                       Coding_by : kippy
+ -- ============================================================================
+*/
+
+/*通用头文件*/
 `include "stddef.h"
+
+/*模块头文件*/
 `include "isa.h"
-module if_reg(input  reset,clk,stall,flush,br_taken
-              input  [29:0] new_pc,br_addr,
-              input  [31:0] insn,
-              output [29:0] if_pc,
-              output [31:0] if_insn,
-              output if_en);
+
+module if_reg(input  clk,               // 时钟
+              input  reset,             // 异步复位
+              input  stall,             // 延迟
+              input  flush,             // 刷新
+              input  br_taken,          // 分支成立
+              input  [29:0] new_pc,     // 新程序计数器值
+              input  [29:0] br_addr,    // 分支目标地址
+              input  [31:0] insn,       // 读取的指令
+              output reg[29:0] if_pc,   // 程序计数器
+              output reg[31:0] if_insn, // 指令
+              output reg if_en          // 流水线数据有效标志位
+              ); 
 
 always @(posedge clk)
     begin
