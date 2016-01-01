b3bc7fd2ee3da8d605b2b2f1121d5cdcd748b36e
beyondskyml <tyust.yun@gmail.com>
2016-01-01 20:35:42 +0800

    add ex_stage.h

---
+++ rtl/include/ex_stage.h
@@ -0,0 +1,18 @@
+/**
+ * filename: ex_stage.h
+ * author  : besky
+ * time    : 2015-12-22 21:08:56
+ */
+
+`ifndef __EX_STAGE_HEADER__
+	`define __EX_STAGE_HEADER__			         // Include Guard
+	
+	`define EX_OUT_SEL_W      2
+	`define EX_OUT_SEL_B      1:0
+
+	`define EX_OUT_ALU        2'b00
+	`define EX_OUT_CMP        2'b01
+	`define EX_OUT_PCN        2'b10
+
+`endif
+
