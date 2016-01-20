86befcad4a6d1d5abc6e6f0d3ddf691b89b85199
kippy <799182081@qq.com>
2016-01-20 11:36:47 +0800

    Add the timescale.

--- rtl/cpu/L2_icache.v
+++ rtl/cpu/L2_icache.v
@@ -7,6 +7,8 @@
  -- ============================================================================
 */
 
+`timescale 1ns/1ps
+
 /********** General header file **********/
 `include "stddef.h"
 `include "icache.h"
