604149f768e1904d37e09d5297b980703db68534
GrinV <colin4124@gmail.com>
2016-03-01 09:18:14 +0800

    complete arguments parser and recognize module name

--- utils/test/test_tbgen.py
+++ utils/test/test_tbgen.py
@@ -6,33 +6,42 @@ class TbgenTest(unittest.TestCase):
     def setUp(self):
         self.tbgen = TestbenchGenerator()
 
+    # Test parser_header_method
     def test_parser_header_method_handle_input_type(self):
-        test_string   ="    input  wire [`GPIO_ADDR_BUS] addr,    // Address"
-        expect_string ="    reg    [`GPIO_ADDR_BUS] addr;    // Address"
+        test_string   = "    input  wire [`GPIO_ADDR_BUS] addr,    // Address"
+        expect_string = "    reg    [`GPIO_ADDR_BUS] addr;    // Address"
 
         result = self.tbgen.parser_header(test_string)
         self.assertEqual(result, expect_string)
 
     def test_parser_header_method_handle_output_type(self):
-        test_string   ="    output reg  [`WORD_DATA_BUS] rd_data, // Read data"
-        expect_string ="    wire   [`WORD_DATA_BUS] rd_data; // Read data"
+        test_string   = "    output reg  [`WORD_DATA_BUS] rd_data, // Read data"
+        expect_string = "    wire   [`WORD_DATA_BUS] rd_data; // Read data"
 
         result = self.tbgen.parser_header(test_string)
         self.assertEqual(result, expect_string)
 
     def test_parser_header_method_handle_inout_type(self):
-        test_string   ="    inout wire [`GPIO_IO_CH-1:0]  gpio_io  // Input/Output Port"
-        expect_string ="    wire  [`GPIO_IO_CH-1:0]  gpio_io  // Input/Output Port"
+        test_string   = "    inout wire [`GPIO_IO_CH-1:0]  gpio_io  // Input/Output Port"
+        expect_string = "    wire  [`GPIO_IO_CH-1:0]  gpio_io  // Input/Output Port"
 
         result = self.tbgen.parser_header(test_string)
         self.assertEqual(result, expect_string)
 
     def test_parser_header_method_handle_module_name(self):
-        test_string   ="module gpio (   inout wire [`GPIO_IO_CH-1:0]  gpio_io );"
-        expect_string ="module gpio_test;    wire  [`GPIO_IO_CH-1:0]  gpio_io ;"
+        test_string   = "module gpio (   inout wire [`GPIO_IO_CH-1:0]  gpio_io );"
+        expect_string = "module gpio_test;    wire  [`GPIO_IO_CH-1:0]  gpio_io ;"
 
         result = self.tbgen.parser_header(test_string)
         self.assertEqual(result, expect_string)
+
+    # Test parser_args_method
+    def test_parser_args_method_return_correct_result(self):
+        test_string = "module gpio ( output reg  [`WORD_DATA_BUS] rd_data, inout wire [`GPIO_IO_CH-1:0]  gpio_io ); // comments"
+        expect_list = ['rd_data', 'gpio_io']
+
+        result = self.tbgen.parser_args(test_string)
+        self.assertEqual(result, expect_list)
 
 if __name__ == '__main__':
 
--- utils/tools/tbgen.py
+++ utils/tools/tbgen.py
@@ -14,7 +14,6 @@ class TestbenchGenerator(object):
             sys.exit(1)
 
     def parser_header(self, line):
-
         # remove 'wire' and 'reg'
         # remove ')' is the same as replacing ');' to ';'
         line = re.sub('\wire|reg|\(|\)', '', line)
@@ -29,33 +28,66 @@ class TestbenchGenerator(object):
 
         # change module_name to module_name_test
         if 'module' in line:
-            line = re.sub(line.split()[1], line.split()[1]+'_test;', line, 1)
+            self.module_name = line.split()[1]
+            line = re.sub(self.module_name, self.module_name+'_test;', line, 1)
 
         return line
 
-    def gen_module_header(self):
-        module_header = ''
+    def parser_args(self, line):
+        args = []
+
+        remove_items = ['input', 'output', 'inout',
+                        'reg', 'wire',
+                        ');', ',', ')', '',
+        ]
+
+        # remove '(' or 'module dut_name ('
+        if '(' in line:
+            line = line.split('(')[1]
+
+        # remove "//" line comments
+        if '//' in line:
+            line = line.split('//')[0]
+
+        line = line.split()
+
+        for item in line:
+            if item not in remove_items and '[' not in item:
+                # remove ',' from 'arg,'
+                item = re.sub(',', '', item)
+                # remove ')' from 'arg)'
+                item = re.sub('\\)', '', item)
+                # remove ');' from 'arg);'
+                item = re.sub('\\);', '', item)
+                args.append(item)
+
+        return args
+
+    def parser(self):
+        self.module_header = ''
+        self.args_list = []
 
         for line in self.src_file_content:
+            if 'input' in line or 'output' in line or 'inout' in line:
+                self.args_list += self.parser_args(line)
 
             # break when read to module header end
             if ');' in line:
-                module_header += self.parser_header(line)
+                self.module_header += self.parser_header(line)
                 break
 
-            module_header += self.parser_header(line)
-
-        return module_header
+            self.module_header += self.parser_header(line)
 
     def write_file(self):
         # Create Testbench File
         with open('gpio_test.v', 'w') as f:
-            f.write(self.gen_module_header())
+            f.write(self.module_header)
 
 if __name__ == "__main__":
 
     tbg = TestbenchGenerator()
     tbg.open_file('gpio.v')
+    tbg.parser()
     tbg.write_file()
 
 
