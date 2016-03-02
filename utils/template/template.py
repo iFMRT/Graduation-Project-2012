faa03ac1819c6ad91f79d5f5c5647b928e46a355
GrinV <colin4124@gmail.com>
2016-03-02 10:34:37 +0800

    Complete Fragment class property test and generate fragment objects test

--- utils/template/template.py
+++ utils/template/template.py
@@ -1,5 +1,11 @@
 import re
 
+# Fragment Type
+VAR_FRAGMENT         = 0
+OPEN_BLOCK_FRAGMENT  = 1
+CLOSE_BLOCK_FRAGMENT = 2
+TEXT_FRAGMENT        = 3
+
 # Split variable token "{{..}}" and block token "{%...%}"
 VAR_TOKEN_START   = '{{'
 VAR_TOKEN_END     = '}}'
@@ -13,6 +19,54 @@ TOK_REGEX = re.compile(r"(%s.*?%s|%s.*?%s)" % (
     BLOCK_TOKEN_END
 ))
 
+class _Fragment(object):
+    def __init__(self, raw_text):
+        self.raw = raw_text
+
+    @property
+    def text(self):
+        # "{{ foo }}" --> "foo"
+        # "{% each bar %}" --> "each bar"
+        if self.raw[:2] in (VAR_TOKEN_START, BLOCK_TOKEN_START):
+            return self.raw[2:-2].strip()
+        return self.raw
+
+    @property
+    def type(self):
+        raw_start = self.raw[:2]
+        if raw_start == VAR_TOKEN_START:
+            return VAR_FRAGMENT
+        elif raw_start == BLOCK_TOKEN_START:
+            return CLOSE_BLOCK_FRAGMENT if self.text[:3] == 'end' else OPEN_BLOCK_FRAGMENT
+        else:
+            return TEXT_FRAGMENT
+
+
+# Just for test
+TestFragment = _Fragment
+
+class _Node(object):
+    def __init__(self, fragment):
+        pass
+
+class _Each(_Node):
+    pass
+
+# Just for test
+TestEach = _Each
+
+class _Text(_Node):
+    pass
+
+# Just for test
+TestText = _Text
+
+class _Variable(_Node):
+    pass
+
+# Just for test
+TestVariable = _Variable
+
 class Compiler(object):
     def __init__(self, template_string):
         self.template_string = template_string
@@ -20,4 +74,22 @@ class Compiler(object):
     def each_fragment(self):
         for fragment in TOK_REGEX.split(self.template_string):
             if fragment:
-                yield fragment
+                yield _Fragment(fragment)
+
+    def create_node(self, fragment):
+        node_class = None
+        if fragment.type == TEXT_FRAGMENT:
+            node_class = _Text
+        elif fragment.type == VAR_FRAGMENT:
+            node_class = _Variable
+        elif fragment.type == OPEN_BLOCK_FRAGMENT:
+            cmd = fragment.text[0]
+            if cmd == 'each':
+                node_class = _Each
+
+        # TODO
+        # if node_class is None:
+        #    raise TemplateSyntaxError(fragment)
+
+        # return node_class(fragment.text)
+
--- utils/template/test_template.py
+++ utils/template/test_template.py
@@ -1,12 +1,39 @@
 import unittest
 from .template import Compiler
+from .template import TestFragment, TestEach, TestText, TestVariable
+from .template import VAR_FRAGMENT, OPEN_BLOCK_FRAGMENT, CLOSE_BLOCK_FRAGMENT, TEXT_FRAGMENT
+
+class FragmentTest(unittest.TestCase):
+    def test_fragment_object_has_correct_property(self):
+        open_obj  = TestFragment("{% each [1, 2, 3] %}")
+        text_obj  = TestFragment("<div>")
+        var_obj   = TestFragment("{{ var }}")
+        close_obj = TestFragment("{% end %}")
+
+        self.assertEqual(open_obj.text, "each [1, 2, 3]")
+        self.assertEqual(open_obj.type, OPEN_BLOCK_FRAGMENT)
+        self.assertEqual(text_obj.text, "<div>")
+        self.assertEqual(text_obj.type, TEXT_FRAGMENT)
+        self.assertEqual(var_obj.text, "var")
+        self.assertEqual(var_obj.type, VAR_FRAGMENT)
+        self.assertEqual(close_obj.text, "end")
+        self.assertEqual(close_obj.type, CLOSE_BLOCK_FRAGMENT)
 
 class CompilerTest(unittest.TestCase):
     def setUp(self):
         self.compiler = Compiler("{%each [1, 2, 3]%} <div> {{var}} </div> {%end%}")
-    def test_each_fragment_method_split_fragments(self):
-        result_list = []
-        for item in self.compiler.each_fragment():
-            result_list.append(item)
+        self.each_fragment = self.compiler.each_fragment()
 
-        self.assertEquals(result_list, ["{%each [1, 2, 3]%}", " <div> ", "{{var}}", " </div> ", "{%end%}"])
+    def test_each_fragment_method_generate_fragment_objects(self):
+        for item in self.each_fragment:
+            self.assertIsInstance(item, TestFragment)
+
+    def test_create_node_method_return_correct_result(self):
+        node_objs = []
+        for item in self.each_fragment:
+            node_objs.append(self.compiler.create_node(item))
+
+        # self.assertIsInstance(node_objs[0], TestEach)
+        # self.assertIsInstance(node_objs[1], TestText)
+        # self.assertIsInstance(node_objs[2], TestVariable)
+        # self.assertIsInstance(node_objs[3], TestText)
