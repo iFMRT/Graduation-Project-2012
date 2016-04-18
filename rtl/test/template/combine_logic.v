52e0626e172f06cb886f3b9eb074998017c46bac
GrinV <colin4124@gmail.com>
2016-04-18 20:34:12 +0800

    add combine logic and sequential logic testbench templates

---
+++ rtl/test/template/combine_logic.v
@@ -0,0 +1,37 @@
+/******** Time scale ********/
+`timescale 1ns/1ps
+{{ header }}
+    /******** Define Simulation Loop********/
+    parameter             STEP = 10;
+
+    {{ dut }}
+
+    task {{ task_name }};{% for it in width_ports %}
+        input {{ it }};{% endfor %}
+
+        begin
+            if({% for it in ports%}({{ it }}  === _{{ it }})  &&
+               {% endfor %}({{ last_port }}  === _{{ last_port }})
+              ) begin
+                $display("Test Succeeded !");
+            end else begin
+                $display("Test Failed !");
+            end
+        end
+
+    endtask
+
+    /******** Test Case ********/
+    initial begin
+        # 0 begin
+            {% for item in first_case  %}{{item}}
+            {% endfor %}end
+        # (STEP * 3/4){% for case_pair in testcase  %}
+        # STEP begin{% for case in case_pair  %}
+            {% for item in case  %}{{item}}
+           {% endfor %}{% endfor %}end
+        {% endfor %}# STEP begin
+            {% for item in last_case  %}{{item}}
+            {% endfor %}$finish;
+        end
+    end{{ footer }}
